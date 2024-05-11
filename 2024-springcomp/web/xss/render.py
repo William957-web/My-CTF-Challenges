309969c210a537296ceb637b4f726ba520766593
William957-web <85293841+William957-web@users.noreply.github.com>
2024-05-12 07:28:22 +0800

    Update render.py

--- 2024-springcomp/web/xss/render.py
+++ 2024-springcomp/web/xss/render.py
@@ -1,7 +1,6 @@
-import asyncio
 from flask import Flask, request, render_template, make_response
-from pyppeteer import launch
-
+import time
+import subprocess
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,17 +19,8 @@ def visit():
     if request.method == 'POST':
         content = request.form['content']
         content = content.replace('%3C', '').replace('%3c', '').replace('%3E', '').replace('%3e', '').replace('<', '').replace('>', '')
-
-        async def run():
-            browser = await launch(headless=True)
-            page = await browser.newPage()
-            await page.goto("https://iced-2024comp-xss-onrender-com.onrender.com/g3tcookieeee")
-            await asyncio.sleep(5)
-            await page.goto("https://iced-2024comp-xss-onrender-com.onrender.com/?content="+content)
-            await asyncio.sleep(5)
-            await browser.close()
-            return f"<h1>Admin have visited it!</h1><br>Final URL: {'https://iced-2024comp-xss-onrender-com.onrender.com/?content='+content}</br>"
-        asyncio.get_event_loop().run_until_complete(run())
+        subprocess.run(['chromium', 'https://iced-2024comp-xss-onrender-com.onrender.com/g3tcookieeee?content='+content, '--headless' ,'--disable-gpu', '--dump-dom', '--no-sandbox'])
+        return f"<h1>Admin have visited it!</h1><br>Final URL: {'https://iced-2024comp-xss-onrender-com.onrender.com/?content='+content}</br>"
     else:
         return "Method not allowed"
 
